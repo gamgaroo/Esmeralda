168b5a707c9b325491da46f6eabbb41277d21648
David Orbelian <david.orbelian@gamgaroo.com>
2018-05-14 04:33:36 +0400

    Fix issues with build scripts.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 set -ex
 
+dotnet restore
+dotnet build
+
+dotnet publish ./src/Gamgaroo.Esmeralda.App -c Release -o ./bin/Docker
+
 docker build -t $USERNAME/$IMAGE:latest ./src/Gamgaroo.Esmeralda.App --no-cache
